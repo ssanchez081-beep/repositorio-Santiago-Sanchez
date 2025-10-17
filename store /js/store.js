021b6f5dd13cd5abe2654cc0a36af44e0f53e90a
Santiago Sánchez L <s.sanchez081@ebc.edu.mx>
2025-10-17 09:51:32 -0600

    Add files via upload

---
+++ store /js/store.js
@@ -0,0 +1,10 @@
+//Aqui inicia mi script del javascript
+// Accordion//
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
